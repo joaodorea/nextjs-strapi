63b300db930cffb3cbee3dae29be7a3593325ad1
Joao Paulo <joaopdorea@gmail.com>
2021-06-09 20:35:31 -0300

    feat: adding index page styles/contenct

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,20 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>React avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escroto ao lado."
+    />
+    <S.Title>React avançado</S.Title>
+    <S.Description>
+      Typescript, ReactJs, NextJs e Styled Components
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um dev com a tela com códigos"
+    />
+  </S.Wrapper>
 )
 
 export default Main
