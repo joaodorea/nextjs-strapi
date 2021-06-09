92747a2b47f9f91d848176a877eb8c94e2fc7263
Joao Paulo <joaopdorea@gmail.com>
2021-06-09 20:35:59 -0300

    test: testing the background of the wrapper

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -12,4 +12,10 @@ describe('<Main />', () => {
 
     expect(container.firstChild).toMatchSnapshot()
   })
+
+  it('Should render the colors correctly', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
+  })
 })
