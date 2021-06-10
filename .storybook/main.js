70b95e52d33e498228bc9c5eb276b191a8451c13
Joao Paulo <joaopdorea@gmail.com>
2021-06-09 21:15:54 -0300

    chore: adding storybook with knobs

---
+++ .storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "stories": [
+    "../src/components/**/stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-knobs/register",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,15 +1,16 @@
 import * as S from './styles'
 
-const Main = () => (
+const Main = ({
+  title = 'React avançado',
+  description = 'Typescript, ReactJs, NextJs e Styled Components'
+}) => (
   <S.Wrapper>
     <S.Logo
       src="img/logo.svg"
       alt="Imagem de um átomo e React Avançado escroto ao lado."
     />
-    <S.Title>React avançado</S.Title>
-    <S.Description>
-      Typescript, ReactJs, NextJs e Styled Components
-    </S.Description>
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
     <S.Illustration
       src="/img/hero-illustration.svg"
       alt="Um dev com a tela com códigos"
